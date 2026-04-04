cf59eebada78033531c110b2b3895f4c59eb75bd
aashikur <mdaashikur@gmail.com>
2026-04-04 15:42:24 +0600

    06.js

---
+++ 01-javascript/module-2-data-transformation-aggregation/examples/06.js
@@ -0,0 +1,44 @@
+
+// ### 6. Social Media Like Aggregator
+// **Description:**
+// Aggregate total likes per post. Each event represents one user liking a post.
+
+// **Function Signature:**
+// ```javascript
+// function aggregateLikes(likes): Object
+// ```
+
+// **Example Input:**
+// ```javascript
+const likes = [
+  { postId: 1, userId: 101 },
+  { postId: 1, userId: 102 },
+  { postId: 2, userId: 101 },
+  { postId: 1, userId: 103 },
+  { postId: 3, userId: 102 }
+];
+// ```
+
+// **Expected Output:**
+// ```javascript
+// { 1: 3, 2: 1, 3: 1 }
+// ```
+
+// **Constraints:** Duplicate likes counted separately | All likes are valid
+
+// **Difficulty:** Easy | **Topics:** `reduce`, aggregation
+
+
+const aggregateLikes = (likes) => {
+    const x = likes.reduce((acc, item) => {
+        const {postId} = item;
+        if(!acc[postId]) {
+            acc[postId] = 0;
+        } 
+        acc[postId] += 1;
+        return acc;
+    }, {})
+    return x;
+}
+
+console.log(aggregateLikes(likes))
